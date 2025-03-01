0e61f7b007096366c7da71aac4ca744123d9cfbb
Bipin87 <sahubipin484@gmail.com>
2025-03-01 12:46:03 +0530

    [Bipin Kumar Sahu] Add: Initialized Address Book Project Using STS added✨

--- AddressBookAppDevelopment/src/main/java/com/capgemini/addressbookappdevelopment/AddressBookAppDevelopmentApplication.java
+++ AddressBookAppDevelopment/src/main/java/com/capgemini/addressbookappdevelopment/AddressBookAppDevelopmentApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AddressBookAppDevelopmentApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(AddressBookAppDevelopmentApplication.class, args);
+		
 	}
 
 }
